347d5f84d41991b5ff7d60727a767064c1eddc73
Max Stoumen <max@mxs.dev>
2023-09-21 16:59:48 -0700

    basic working state

---
+++ output/csharp/src/Co.Seam.Test/Client/SeamTests.cs
@@ -0,0 +1,84 @@
+namespace Co.Seam.Test;
+
+using Co.Seam.Client;
+using Co.Seam.Model;
+
+public class UnitTest1 : SeamConnectTest
+{
+    [Fact]
+    public void TestGetDeviceList()
+    {
+        var device = seam.Devices
+            .List()
+            .First(d => d.DeviceType == Device.DeviceTypeEnum.AugustLock);
+
+        Assert.NotNull(device);
+        Assert.Equal("Front Door", device.Properties.Name);
+    }
+
+    [Fact]
+    public async void TestGetDeviceListAsync()
+    {
+        var device = (await seam.Devices.ListAsync()).First(
+            d => d.DeviceType == Device.DeviceTypeEnum.AugustLock
+        );
+
+        Assert.NotNull(device);
+        Assert.Equal("Front Door", device.Properties.Name);
+    }
+
+    [Fact]
+    public void TestCreateAccessCode()
+    {
+        var device = seam.Devices
+            .List()
+            .First(d => d.DeviceType == Device.DeviceTypeEnum.AugustLock);
+
+        var access_code = seam.AccessCodes.Create(deviceId: device.DeviceId, code: "1234");
+
+        Assert.Equal(AccessCode.StatusEnum.Setting, access_code.Status);
+        Assert.Equal("1234", access_code.Code);
+    }
+
+    [Fact]
+    public async void TestCreateAccessCodeAsync()
+    {
+        var device = seam.Devices
+            .List()
+            .First(d => d.DeviceType == Device.DeviceTypeEnum.AugustLock);
+
+        var access_code = await seam.AccessCodes.CreateAsync(
+            deviceId: device.DeviceId,
+            code: "1234"
+        );
+
+        Assert.Equal(AccessCode.StatusEnum.Setting, access_code.Status);
+        Assert.Equal("1234", access_code.Code);
+    }
+
+    [Fact]
+    public void TestLockDoor()
+    {
+        var device = seam.Devices
+            .List()
+            .First(d => d.DeviceType == Device.DeviceTypeEnum.AugustLock);
+
+        var action_attempt = seam.Locks.LockDoor(deviceId: device.DeviceId);
+
+        Assert.IsType<ActionAttemptPending>(action_attempt);
+        Assert.Equal("LOCK_DOOR", (action_attempt as ActionAttemptPending)!.ActionType);
+    }
+
+    [Fact]
+    public async void TestLockDoorAsync()
+    {
+        var device = seam.Devices
+            .List()
+            .First(d => d.DeviceType == Device.DeviceTypeEnum.AugustLock);
+
+        var action_attempt = await seam.Locks.LockDoorAsync(deviceId: device.DeviceId);
+
+        Assert.IsType<ActionAttemptPending>(action_attempt);
+        Assert.Equal("LOCK_DOOR", (action_attempt as ActionAttemptPending)!.ActionType);
+    }
+}
---
+++ output/csharp/src/Co.Seam.Test/SeamConnectTest.cs
@@ -0,0 +1,30 @@
+namespace Co.Seam.Test;
+
+using Co.Seam.Client;
+
+public class SeamConnectTest : IDisposable
+{
+    public Seam seam;
+
+    private static readonly Random random = new();
+
+    public SeamConnectTest()
+    {
+        var r = new string(
+            Enumerable
+                .Repeat("ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789", 10)
+                .Select(s => s[random.Next(s.Length)])
+                .ToArray()
+        );
+
+        seam = new Seam(
+            basePath: string.Format("https://{0}.fakeseamconnect.seam.vc", r),
+            apiToken: "seam_apikey1_token"
+        );
+    }
+
+    public void Dispose()
+    {
+        seam.Dispose();
+    }
+}
